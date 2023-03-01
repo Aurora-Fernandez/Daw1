5404f99916b2c71b816d91655962efef3c0d7324
Juan Alvarez <j.alvarezl.2019@alumnos.urjc.es>
2023-03-01 16:10:03 +0100

    Añadida una clase para pruebas

---
+++ appWeb/src/main/java/es/codeurjc/web/Book.java
@@ -0,0 +1,49 @@
+package es.codeurjc.web;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book {
+
+    private long id;
+    private String name;
+    private String description;
+    private String genre;
+
+
+    protected Book(){}
+
+    public Book(String name, String description, String genre) {
+        this.name = name;
+        this.description = description;
+        this.genre = genre;
+    }
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+    
+
+    
+}
